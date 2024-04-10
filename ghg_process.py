9bfa15a3102cb4d89affa3c3a597f5db6c268cc0
Jay Fahlen <jay.e.fahlen@jpl.nasa.gov>
2024-04-10 11:30:47 -0700

    Clean up parallel_mf calls

--- ghg_process.py
+++ ghg_process.py
@@ -72,8 +72,14 @@ def main(input_args=None):
     co2_mf_file = f'{args.output_base}_co2_mf'
     ch4_mf_file = f'{args.output_base}_ch4_mf'
 
+    # Uncertainty
+    co2_mf_uncert_file = f'{args.output_base}_co2_mf_uncert'
     ch4_mf_uncert_file = f'{args.output_base}_ch4_mf_uncert'
+
+    # Sensitivity
+    co2_mf_sens_file = f'{args.output_base}_co2_mf_sensitivity'
     ch4_mf_sens_file = f'{args.output_base}_ch4_mf_sensitivity'
+
     emit_noise_parameters_file = f'./instrument_noise_parameters/emit_noise.txt'
 
     # Flares
@@ -152,7 +158,9 @@ def main(input_args=None):
 
 
     if (os.path.isfile(co2_mf_file) is False or args.overwrite) and args.co2:
-        subargs = [args.radiance_file, co2_target_file, co2_mf_file, '--n_mc', '1', '--l1b_bandmask_file', args.l1b_bandmask_file, '--l2a_mask_file', args.l2a_mask_file, '--wavelength_range', '1900', '2350', '--fixed_alpha', '0.0000000001', '--mask_clouds_water']
+        subargs = [args.radiance_file, co2_target_file, co2_mf_file, '--n_mc', '1', '--l1b_bandmask_file', args.l1b_bandmask_file, '--l2a_mask_file', args.l2a_mask_file, '--wavelength_range', '500', '1340', '1500', '1790', '1950', '2450', \
+                   '--fixed_alpha', '0.0000000001', '--mask_clouds_water', '--flare_outfile', flare_file, \
+                   '--noise_parameters_file', emit_noise_parameters_file, '--sens_output_file', co2_mf_sens_file, '--uncert_output_file', co2_mf_uncert_file]
         if args.ace_filter:
             subargs.append('--use_ace_filter')
         parallel_mf.main(subargs)
@@ -160,14 +168,7 @@ def main(input_args=None):
     if os.path.isfile(ch4_mf_file) is False or args.overwrite:
         logging.info('starting parallel mf')
         
-        #subargs = [args.radiance_file, ch4_target_file, ch4_mf_file, ch4_mf_uncert_file, ch4_mf_sens_file, '--n_mc', '1', '--l1b_bandmask_file', args.l1b_bandmask_file, '--l2a_mask_file', args.l2a_mask_file, \
-        #           '--wavelength_range', '500', '1340', '1500', '1790', '1950', '2450', '--fixed_alpha', '0.0000000001', '--mask_clouds_water', '--mask_flares', '--flare_outfile', flare_file, \
-
-        #           '--noise_parameters_file', '/beegfs/scratch/jfahlen/emit_radiance_model/emit_noise.txt']
-
-        #subargs = [args.radiance_file, ch4_target_file, ch4_mf_file, '--n_mc', '1', '--l1b_bandmask_file', args.l1b_bandmask_file, '--l2a_mask_file', args.l2a_mask_file, '--wavelength_range', '500', '2450', '--fixed_alpha', '0.0000000001', '--mask_clouds_water', '--flare_outfile', flare_file]
-        #subargs = [args.radiance_file, ch4_target_file, ch4_mf_file, '--n_mc', '1', '--l1b_bandmask_file', args.l1b_bandmask_file, '--l2a_mask_file', args.l2a_mask_file, '--wavelength_range', '500', '1340', '1500', '1790', '1950', '2450', \
-        subargs = [args.radiance_file, ch4_target_file, ch4_mf_file, '--n_mc', '1', '--l1b_bandmask_file', args.l1b_bandmask_file, '--l2a_mask_file', args.l2a_mask_file, '--wavelength_range', '500', '1340', '1500', '1790', '2030', '2450', \
+        subargs = [args.radiance_file, ch4_target_file, ch4_mf_file, '--n_mc', '1', '--l1b_bandmask_file', args.l1b_bandmask_file, '--l2a_mask_file', args.l2a_mask_file, '--wavelength_range', '500', '1340', '1500', '1790', '1950', '2450', \
                    '--fixed_alpha', '0.0000000001', '--mask_clouds_water', '--flare_outfile', flare_file, \
                    '--noise_parameters_file', emit_noise_parameters_file, '--sens_output_file', ch4_mf_sens_file, '--uncert_output_file', ch4_mf_uncert_file]
 
