c1045322acd72a96589861c1453714e95362dcb3
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2024-06-21 09:56:14 -0700

    Use ch4/co2 flag to find cogs

--- find_cogs_to_deliver.py
+++ find_cogs_to_deliver.py
@@ -1,8 +1,19 @@
 import glob
 import os
+import sys
 
-cogs = glob.glob(f"/scratch/brodrick/methane/visions_delivery/20*/*/*tif")
-cmr_files = glob.glob(f"/scratch/brodrick/methane/visions_delivery/20*/*/*cmr.json")
+if len(sys.argv) == 1:
+    print("Must specify either 'ch4' or 'co2' as an argument")
+    sys.exit(1)
+ghg =  sys.argv[1]
+
+if ghg == "ch4":
+    cogs = glob.glob(f"/scratch/brodrick/methane/visions_delivery_20240409/20*/*/*tif")
+    cmr_files = glob.glob(f"/scratch/brodrick/methane/visions_delivery_20240409/20*/*/*cmr.json")
+if ghg == "co2":
+    cogs = glob.glob(f"/scratch/brodrick/methane/visions_delivery_co2/20*/*/*tif")
+    cmr_files = glob.glob(f"/scratch/brodrick/methane/visions_delivery_co2/20*/*/*cmr.json")
+
 basenames = [os.path.basename(f).replace(".cmr.json", "") for f in cmr_files]
 
 new_cogs = []
