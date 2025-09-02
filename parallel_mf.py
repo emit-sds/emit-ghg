4bd41d344efbdd9afb25fb804525e5e56a6922de
Jay E. Fahlen <jay.e.fahlen@jpl.nasa.gov>
2025-09-02 12:03:32 -0700

    Fix emit vs EMIT

--- parallel_mf.py
+++ parallel_mf.py
@@ -125,7 +125,7 @@ def main(input_args=None):
         la = np.where(np.logical_and(wavelengths > args.wavelength_range[2*n], wavelengths <= args.wavelength_range[2*n+1]))[0]
         active_wl_idx.extend(la.tolist())
     always_exclude_idx = []
-    if 'emit' in args.radiance_file:
+    if 'emit' in args.radiance_file.lower():
         always_exclude_idx = np.where(np.logical_and(wavelengths < 1321, wavelengths > 1275))[0].tolist()
     active_wl_idx = np.array([x for x in active_wl_idx if x not in always_exclude_idx])
 
@@ -227,6 +227,8 @@ def main(input_args=None):
         del radiance
 
         logging.info('Run jobs')
+        print(active_wl_idx)
+        pdb.set_trace()
         jobs = [mf_one_column.remote(col, rdn_id, absorption_coefficients_id, active_wl_idx, good_pixel_mask, noise_model_parameters_id, args) for col in range(output_shape[2])]
         rreturn = [ray.get(jid) for jid in jobs]
 
