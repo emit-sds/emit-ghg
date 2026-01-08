835fb72f0c0650dee9802d4dd07aa2a5274a122e
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2026-01-08 12:02:20 -0800

    Add copyright to header

--- daac_delivery.py
+++ daac_delivery.py
@@ -1,8 +1,20 @@
-"""
-Delivers L2B CH4/CO2 Enhancement Data and Plumes to the DAAC
-
-Author: Winston Olson-Duvall, winston.olson-duvall@jpl.nasa.gov
-"""
+#! /usr/bin/env python
+#
+#  Copyright 2023 California Institute of Technology
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+# Author: Winston Olson-Duvall, winston.olson-duvall@jpl.nasa.gov
 
 import argparse
 import datetime
